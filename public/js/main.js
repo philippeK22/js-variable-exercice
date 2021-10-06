d7d7683ada90e9698958657c25eca80c72fcafa1
philippe <kanengelephilippe@gmail.com>
2021-10-06 14:54:06 +0200

    fin exercice

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+// // Exo1
+// let nombre = 10;
+// let nombre2 = 5;
+// console.log(`${nombre} + ${nombre2} = ${nombre +nombre2} `);
+// console.log(`${nombre} - ${nombre2} = ${nombre - nombre2}`);
+// console.log(`${nombre} * ${nombre2} = ${nombre * nombre2}`);
+// console.log(`${nombre} / ${nombre2} = ${nombre / nombre2}`);
+
+// // exercice 2
+
+// let age = Number(prompt('quel est ton age ?'));
+// let numberConvert = parseInt(age);
+// console.log(typeof age);
+// let prenom = prompt("quel est ton prenom ?");
+// console.log(`${prenom} a ${age} ans cette année`);
+// console.log(`${prenom} aura ${age+3} dans 3ans`);
+
+// // exercice3
+
+// let annee = 1995;
+// let actuelle = 2021;
+// console.log(`ton age actuelle : ${actuelle - annee} ans`);
+// console.log(`ton age dans deux ans : ${(actuelle - annee) + 2} ans`);
